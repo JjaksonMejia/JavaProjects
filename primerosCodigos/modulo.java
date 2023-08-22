ad0b4e7b8fc300f6183826489b7a6a825c76738b
JjaksonMejia <142945239+JjaksonMejia@users.noreply.github.com>
2023-08-22 17:30:32 -0500

    Add files via upload

---
+++ primerosCodigos/modulo.java
@@ -0,0 +1,7 @@
+public class modulo {
+    public static void main(String args[]){
+        int estudiantes = 26;
+        int leftOut = estudiantes%3;
+        System.out.println(leftOut);
+    }
+}
