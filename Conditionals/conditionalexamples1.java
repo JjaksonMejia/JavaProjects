36b118a6a039e50f76069f8b8a76ac34cea833a1
JjaksonMejia <142945239+JjaksonMejia@users.noreply.github.com>
2023-09-06 10:47:03 -0500

    Add files via upload

---
+++ Conditionals/conditionalexamples1.java
@@ -0,0 +1,10 @@
+//package Conditionals;
+public class conditionalexamples1{
+    public static void main(String[] args){
+        int x = 10;
+        int y = 12;
+        if(x+y > 20){
+            System.out.println("X + y is greather than 20");
+        }
+    }
+}
---
+++ Conditionals/conditionalexamples2.java
@@ -0,0 +1,14 @@
+//package Conditionals;
+
+public class conditionalexamples2 {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 12;
+
+        if (x + y < 10){
+            System.out.println("X + y is less than 10");
+        } else {
+            System.out.println("X + y is greather than 20");
+        }
+    }
+}
