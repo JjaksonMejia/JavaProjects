6ffc131a600a37ec0e0086b912f773e47dd30fb0
JjaksonMejia <142945239+JjaksonMejia@users.noreply.github.com>
2023-09-07 17:11:56 -0500

    Add files via upload

---
+++ encapsulamientoYAcceso/bank/Bank.java
@@ -0,0 +1,16 @@
+//package bank.encapsulamientoYAcceso;
+public class Bank{
+	private CheckingAccount accountOne;
+	//private CheckingAccount accountTwo;
+	
+	public Bank(){
+		accountOne = new CheckingAccount("Zeus", 100);
+		accountOne = new CheckingAccount("hades", 400);
+	}
+
+	public static void main(String[] args){
+		Bank bankOfGods = new Bank();
+		System.out.println(bankOfGods.accountOne.name);
+		System.out.println(bankOfGods.accountOne.balance);
+	}
+}
---
+++ encapsulamientoYAcceso/bank/CheckingAccount.java
@@ -0,0 +1,10 @@
+//package bank.encapsulamientoYAcceso;
+public class CheckingAccount{
+	public String name;
+	public int balance;
+
+	public CheckingAccount(String inputName, int inputBalance){
+		name = inputName;
+		balance = inputBalance;
+	}
+}
