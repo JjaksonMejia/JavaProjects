1b18b3c97e3d4f4a8a3007d8f08c6b9dce4cefe5
JjaksonMejia <142945239+JjaksonMejia@users.noreply.github.com>
2023-09-11 21:39:13 -0500

    Add files via upload

---
+++ Loops/structureBreak/example4.java
@@ -0,0 +1,14 @@
+//package Loops.structureBreak;
+
+public class example4 {
+    public static void main(String[] args) {  
+        int i=1;  
+        while(i<=10){  
+            if(i==5){   
+                i++;  
+            }  
+            System.out.println(i);  
+            i++;  
+        }  
+    }  
+}
