b934ce91eff111a2a7855865e922430e0ee7f04f
JjaksonMejia <142945239+JjaksonMejia@users.noreply.github.com>
2023-09-11 21:28:37 -0500

    Add files via upload

---
+++ Loops/loopWhile/infinityWhile.java
@@ -0,0 +1,9 @@
+//package Loops.loopWhile;
+
+public class infinityWhile {
+    public static void main(String[] args) {    
+           while(true){    
+               System.out.println("infinity while loop");    
+           }    
+       } 
+}
